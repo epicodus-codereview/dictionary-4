5734f72b8588b47df055d5bd516dfd117ec22dda
Guest User <student@epicodus.com>
2015-08-14 12:37:57 -0700

    updated integration spec

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -9,5 +9,9 @@ describe('dictionary path', {:type => :feature}) do
     fill_in('word', :with => 'cat')
     click_button('Add Word')
     expect(page).to have_content('Cat')
+    click_link('Cat')
+    fill_in('definition', :with => 'feline animal')
+    click_button('Add')
+    expect(page).to have_content('Cat Feline animal Add a definition for this word Add Home')
   end
 end

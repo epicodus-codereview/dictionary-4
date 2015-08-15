88ad52efb6a59735ebfb3d4f8e6b58a23dc19d9c
Mike Goren <mgoren@mortalwombat.net>
2015-08-15 15:06:36 -0700

    code review feedback

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,9 +2,9 @@ require('rspec')
 require('definition')
 
 describe('Definition') do
-  before do
-    Definition.clear
-  end
+  # before do
+  #   Definition.clear
+  # end
 
   describe("#definition") do
     it('returns definition') do
@@ -13,48 +13,48 @@ describe('Definition') do
     end
   end
 
-  describe("#save") do
-    it('adds definition to class array') do
-      test_definition = Definition.new('break apart')
-      test_definition.save()
-      expect(Definition.all()).to eq([test_definition])
-    end
-  end
+  # describe("#save") do
+  #   it('adds definition to class array') do
+  #     test_definition = Definition.new('break apart')
+  #     test_definition.save()
+  #     expect(Definition.all()).to eq([test_definition])
+  #   end
+  # end
 
-  describe(".all") do
-    it('returns list of definitions') do
-      expect(Definition.all()).to eq([])
-    end
-  end
+  # describe(".all") do
+  #   it('returns list of definitions') do
+  #     expect(Definition.all()).to eq([])
+  #   end
+  # end
 
-  describe('.clear') do
-    it('clears list of words') do
-      test_definition = Definition.new('to randomize')
-      test_definition2 = Definition.new('excessively angry')
-      test_definition.save
-      test_definition2.save
-      Definition.clear()
-      expect(Definition.all()).to eq([])
-    end
-  end
+  # describe('.clear') do
+  #   it('clears list of words') do
+  #     test_definition = Definition.new('to randomize')
+  #     test_definition2 = Definition.new('excessively angry')
+  #     test_definition.save
+  #     test_definition2.save
+  #     Definition.clear()
+  #     expect(Definition.all()).to eq([])
+  #   end
+  # end
 
-  describe('#id') do
-    it('returns definition id when called') do
-      test_definition = Definition.new('to police')
-      test_definition.save
-      test_definition2 = Definition.new('excessively angry')
-      test_definition2.save
-      expect(test_definition2.id).to eq(2)
-    end
-  end
+  # describe('#id') do
+  #   it('returns definition id when called') do
+  #     test_definition = Definition.new('to police')
+  #     test_definition.save
+  #     test_definition2 = Definition.new('excessively angry')
+  #     test_definition2.save
+  #     expect(test_definition2.id).to eq(2)
+  #   end
+  # end
 
-  describe('.find') do
-    it('finds and returns word in array based on ID number') do
-      test_definition = Definition.new('to police')
-      test_definition.save
-      test_definition2 = Definition.new('tiger')
-      test_definition2.save
-      expect(Definition.find(2)).to eq(test_definition2)
-    end
-  end
+  # describe('.find') do
+  #   it('finds and returns word in array based on ID number') do
+  #     test_definition = Definition.new('to police')
+  #     test_definition.save
+  #     test_definition2 = Definition.new('tiger')
+  #     test_definition2.save
+  #     expect(Definition.find(2)).to eq(test_definition2)
+  #   end
+  # end
 end
